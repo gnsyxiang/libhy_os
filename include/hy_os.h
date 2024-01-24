416db7be97563c5d644c35db505ca601f5768ee7
zhenquan.qiu <gnsyxiang@163.com>
2024-01-24 17:51:15 +0800

    feat: add hy_os.h file

---
+++ include/hy_os.h
@@ -0,0 +1,34 @@
+/**
+ *
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_os.h
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    24/01 2024 17:39
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        24/01 2024      create the file
+ * 
+ *     last modified: 24/01 2024 17:39
+ */
+#ifndef __LIBHY_OS_INCLUDE_HY_OS_H_
+#define __LIBHY_OS_INCLUDE_HY_OS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void HyOsCheck(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ src/hy_os.c
@@ -0,0 +1,27 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_os.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    24/01 2024 17:50
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        24/01 2024      create the file
+ * 
+ *     last modified: 24/01 2024 17:50
+ */
+#include <stdio.h>
+
+#include "hy_os.h"
+
+void HyOsCheck(void)
+{
+    printf("check libhy_os.so \n");
+}
